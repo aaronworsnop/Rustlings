f250202aa7ef191ffda33c9069b6dedf1b6a2850
Aaron <aaronworsnop@gmail.com>
2026-02-16 17:17:43 +1100

    Complete 03 if

---
+++ exercises/03_if/if1.rs
@@ -0,0 +1,38 @@
+use std::cmp::Ordering;
+
+fn bigger(a: i32, b: i32) -> i32 {
+    // If both numbers are equal, any of them can be returned.
+    // Do not use:
+    // - another function call
+    // - additional variables
+    match a.cmp(&b) {
+        Ordering::Greater => a,
+        Ordering::Equal => a,
+        Ordering::Less => b,
+    }
+}
+
+fn main() {
+    println!("{}", bigger(1, 2));
+}
+
+// Don't mind this for now :)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ten_is_bigger_than_eight() {
+        assert_eq!(10, bigger(10, 8));
+    }
+
+    #[test]
+    fn fortytwo_is_bigger_than_thirtytwo() {
+        assert_eq!(42, bigger(32, 42));
+    }
+
+    #[test]
+    fn equal_numbers() {
+        assert_eq!(42, bigger(42, 42));
+    }
+}
